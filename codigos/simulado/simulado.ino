141a210aace3b16a75df6febf7e9a36d7def27ae
Bruno Eduardo <bruno97br@gmail.com>
2018-03-05 15:24:25 -0300

    Ultimo simulado funcionando

--- codigos/simulado/simulado.ino
+++ codigos/simulado/simulado.ino
@@ -16,13 +16,14 @@
 
 
  /*  TODO:
-  *
+  *  resolver problema de iteração num vetor de String
  */
 #define PROFESSOR "165215"                         //RA do professor
 
 //Global Var
 String dataHj;
-
+String listaDePresenca[100];
+int AlunosRegistrados;
 /****************************************************************************/
 
 void setup() {
@@ -55,23 +56,40 @@ String digitalLida(){
     delay(100);
   }
   int caracteres = Serial.available();
+  if(caracteres > 6)
+    caracteres = 6;
   for(int i=0;i<caracteres;i++){
     RA[i] = Serial.read();
   }
+  while(Serial.available() > 0){
+    char flush = Serial.read();
+  }
+
+  Serial.print("Acabei de ler ");
+  Serial.println(RA);
+  
   return RA;
 }
 
 void gravarPresenca(String leitura, String data){
   //Insere o RA e a data da presenca no arquivo aberto
-  Serial.print("Mensagem gravada no cartao: ");
-  Serial.print(leitura);
-  Serial.print(" ");
-  Serial.println(data);
+  if(jaCadastrado(leitura)){
+    report("Presenca ja cadastrada");
+  }
+  else{
+    Serial.print("Mensagem gravada no cartao: ");
+    Serial.print(leitura);
+    Serial.print(" ");
+    Serial.println(data);
+    listaDePresenca[AlunosRegistrados] = leitura;
+    AlunosRegistrados++;
+  }
 }
 
 void iniciarDia(){
-  dataHj = "hoje eh 03";
+  dataHj = word(analogRead(A0));
   report("Inicio do programa");
+  AlunosRegistrados = 0;
 }
 
 void encerrarDia(){
@@ -87,5 +105,26 @@ void report(String message){
 void inicializarPerifericos(){
   String dataHj = "inicio";
   Serial.begin(9600);
+}
+
+bool jaCadastrado(String aluno){
+  report("Impressao da lista de presenca----------");
+  for(int i=0; i<AlunosRegistrados; i++){
+    Serial.println(listaDePresenca[i]);
+  }
+
+  Serial.print("O aluno agora eh ");
+  Serial.println(aluno);
   
+  for(int i=0; i<AlunosRegistrados; i++){
+    if(listaDePresenca[i][4] == aluno[4]);
+      Serial.print(listaDePresenca[i][4]);
+      Serial.print("O aluno lido ");
+      Serial.print(aluno);
+      Serial.print(" é igual o da lista");
+      Serial.print(listaDePresenca[i]);
+      report(aluno);
+      return true;
+  }
+  return false;
 }
